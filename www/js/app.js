a5aeab1ca4c47d7e6c4a2ef2c13746050bd7a75e
pocketjav <pocketjav@hotmail.com>
2017-08-24 09:26:08 -0600

    Primeros cambios al repositorio

---
+++ www/js/app.js
@@ -0,0 +1,28 @@
+//Area de Cuadrado-rectangulo
+var altura = 7;
+var base = 5;
+var area = altura * base;
+console.log("El area de el rectangulo es igual a: "+ area);
+// area es 35
+
+//Area de Circulo
+var radio = 4;
+var pi = 3.1416;
+var areaC = pi * radio * radio;
+console.log("El area del circulo es:"+ areaC );
+// area es 50.2656
+
+//Area de Triangulo
+var altura = 5;
+var base = 7;
+var areaT = base * altura/2;
+console.log("El area del triangulo es"+ areaT);
+// area es = 17.5
+
+//Volumen de una esfera
+var pi = 3.1416;
+var x = 4/3;
+var radio = 4;
+var volumen = pi * x * radio*radio*radio;
+console.log("El Volumen de la esfera es:"+ volumen );
+// volumen es 268.0832
