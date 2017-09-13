6dbed8f966942c087379eb66e2b5e3c313328f40
pocketjav <pocketjav@hotmail.com>
2017-09-13 10:35:29 -0600

    Varias Estrellas al mismo tiempo Tarea

--- LOLAL/www/js/scene.js
+++ LOLAL/www/js/scene.js
@@ -7,12 +7,30 @@ let stars = [];
 for(let i=0; i<numstars; i++){
 let h = i * 360 / numstars;
    stars[i] = star.clone().attr({ 
-   fillColor: 'hsl(' +h +',100%, 50%)'
-   x: width/2;
-   y:height/2;
+   fillColor: 'hsl(' +h +',100%, 50%)',
+   x: width/2,
+   y: height/2
 });
 }
 return stars;
-})(new star(width/2, height/2,1,5));
+})(new Star(width/2, height/2,1,5));
 
-stage.length(14);
+stage.length(1);
+
+let i=0;
+stage.on(0, function(){
+let star = stars[i];
+
+star.attr({
+    scalex: 1,
+    sclaey: 1,
+    opacity: 1
+}).addTo(stage).animate(2*numstars,{
+        scalex: width * 3,
+        scaley: height * 3,
+        opacity : 0
+    },{isTimeLineBound: false});
+
+    i = (i + 1) % numstars;
+    stage.removeChild(star[i]);
+})
