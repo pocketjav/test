339b5cf97ddf16e0153515a056bc272e5a26adbc
pocketjav <pocketjav@hotmail.com>
2017-09-18 10:41:12 -0600

    3 cuadrados moviendose

--- Tararan/www/js/movie.js
+++ Tararan/www/js/movie.js
@@ -1,17 +1,26 @@
 var rectPath = new Rect(10,10,150,150)
-.attr({
-    fillColor: 'blue',
-    strokeColor: 'green',
-    strokeWidth: 5,
-    x: 150,
-    y: 150,
-});
-
-
-stage.addChild(rectPath);
-
-rectPath.animate('2s',{
-    rotation: Math.PI*2
-},{
-    easing: 'elasticInOut'
-})
+.addTo(stage)
+.attr('fillColor', 'blue')  
+.animate('400ms', {
+    x: 400,
+    
+    
+  });
+  var rectPath= new Rect(400,170,150,150)
+  .addTo(stage)
+  .attr('fillColor', 'red')  
+  .animate('400ms', {
+      x: 10,
+  });
+  
+  var rectPath = new Rect(10,300,150,150)
+  .addTo(stage)
+  .attr('fillColor', 'green')  
+  .animate(new KeyframeAnimation(
+      '400ms',{
+          from:{x:10,y:300},
+          to:{x:400,y:10},
+      }
+  )
+       
+    );
