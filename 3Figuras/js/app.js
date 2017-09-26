a21c4df7b42af72794c8014996c02eed0a44ef6a
pocketjav <pocketjav@hotmail.com>
2017-09-26 09:53:45 -0600

    Pong

---
+++ 3Figuras/js/app.js
@@ -0,0 +1,10 @@
+new Rect(10,10,100,100)
+.addTo(stage)
+.attr('fillColor', 'blue')
+
+new Triangle(30.30,100,100,100)
+.addTo(stage)
+.attr('fillColor', 'red')
+
+new Circle(20, 20, 50)
+
--- Tararan/www/js/movie.js
+++ Tararan/www/js/movie.js
@@ -1,9 +1,10 @@
 var rectPath = new Rect(10,10,150,150)
+
 .addTo(stage)
 .attr('fillColor', 'blue')  
 .animate('400ms', {
     x: 400,
-    
+    rotation: Math.PI*2
     
   });
   var rectPath= new Rect(400,170,150,150)
@@ -11,16 +12,27 @@ var rectPath = new Rect(10,10,150,150)
   .attr('fillColor', 'red')  
   .animate('400ms', {
       x: 10,
+      rotation: Math.PI*2
   });
   
-  var rectPath = new Rect(10,300,150,150)
+  var rectpath = new Rect(10,300,150,150)
   .addTo(stage)
   .attr('fillColor', 'green')  
+  
   .animate(new KeyframeAnimation(
       '400ms',{
           from:{x:10,y:300},
           to:{x:400,y:10},
+          
       }
   )
        
     );
+    
+    /*stage.addChild(rectPath);
+    +
+    +rectPath.animate('2s',{
+    +    rotation: Math.PI*2
+    +},{
+    +    easing: 'elasticInOut'
+    +}) */
